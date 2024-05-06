589608d76d44efcd84d4d8c1e33fca078df08c6c
Dean Meyer <102161782+meyer-d3v@users.noreply.github.com>
2024-05-06 15:36:43 +0200

    Add files via upload

---
+++ Python/mario.py
@@ -0,0 +1,16 @@
+def main():
+    height = get_height()
+    for i in range(height):
+        print("#")
+
+def get_height():
+    while True:
+        try:
+            n = int(input("Height: "))
+            if n > 0:
+                return n
+        except ValueError:
+            print("Please enter valid number!")    
+          
+
+main()
---
+++ Python/numbers_1.py
@@ -0,0 +1,11 @@
+import sys
+
+numbers = [4, 6, 8, 2, 7, 5, 0]
+
+if 0 in numbers:
+    print("Found")
+    sys.exit(0)
+
+print("Not found")
+sys.exit(1)
+
