db7ff90fd633a1c8fb6b43d65e755dc3d86dc2fd
Dean Meyer <102161782+meyer-d3v@users.noreply.github.com>
2024-05-31 19:16:43 +0200

    Add files via upload

---
+++ Javascript/Arrays.js
@@ -0,0 +1,8 @@
+let numbers = [1, 2, 3];
+let names = ["Dean", "John", "Nathan"];
+
+console.log(names);
+//Printing second item in array
+console.log(names[1]);
+//printing 3rd item in array
+console.log(numbers[2])
