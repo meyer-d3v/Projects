9a3772b90f38b2efef9fe1089d7e794a6349889c
Dean Meyer <102161782+meyer-d3v@users.noreply.github.com>
2025-03-12 14:54:15 +0200

    Add files via upload
    This is another short program I have been working on. The overall function of the program is to create a shopping receipt for 5 items with its prices for users to see their items in a formal manner.

---
+++ Java/ShopperCalcuator/src/shoppercalcuator/ShopperCalcuator.java
@@ -0,0 +1,81 @@
+package shoppercalcuator;
+import java.util.*;
+
+/**
+ * 
+ * 
+ * @author Dean Christopher Meyer
+ * @studentnr 250742640
+ */
+public class ShopperCalcuator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        String first, second, third, fourth, fifth;
+        double ifirst, isecond, ithird, ifourth, ififth;
+        double totalAmount = 0;
+        
+        
+        System.out.print("Enter the name of the first item you want to buy: ");
+        first = myObj.next();
+        System.out.print("Enter the price of the first item: R");
+        //ifirst = myObj.nextDouble();
+        ifirst = myObj.nextDouble();
+        
+        System.out.print("Enter the name of the second item you want to buy: ");
+        second = myObj.next();
+        System.out.print("Enter the price of the second item: R");
+        isecond = myObj.nextDouble();
+        
+        System.out.print("Enter the name of the third item you want to buy: ");
+        third = myObj.next();
+        System.out.print("Enter the price of the third item: R");
+        ithird = myObj.nextDouble();
+        
+        System.out.print("Enter the name of the fourth item you want to buy: ");
+        fourth = myObj.next();
+        System.out.print("Enter the price of the fourth item: R");
+        ifourth = myObj.nextDouble();
+        
+        System.out.print("Enter the name of the fifth item you want to buy: ");
+        fifth = myObj.next();
+        System.out.print("Enter the price of the fifth item: R");
+        
+        ififth = myObj.nextDouble();
+        
+        System.out.println("===================================================");
+        System.out.println("You have selected to buy the folowing items: ");
+        
+        System.out.println("---------------------------------------------------");
+        
+        System.out.println("Item: " + first + " at R" + ifirst);
+        System.out.println("Item: " + second + " at R" + isecond);
+        System.out.println("Item: " + third + " at R" + ithird);
+        System.out.println("Item: " + fourth + " at R" + ifourth);
+        System.out.println("Item: " + fifth + " at R" + ififth);
+        
+        System.out.println("---------------------------------------------------");
+        
+        double total = ifirst + isecond + ithird + ifourth + ififth;
+        System.out.println("Total for all items : R" + total);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
